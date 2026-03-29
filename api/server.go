f15988aea98e04a16ccb4c412397febdd95a0159
river <umnaser1974@gmail.com>
2026-03-29 17:55:30 +0300

    refactor(api): created custom currency validator, and renamed api files to plural form

--- api/server.go
+++ api/server.go
@@ -2,8 +2,12 @@
 package api
 
 import (
+	"log"
+
 	db "github.com/HyperNaser/gobank/db/sqlc"
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/validator/v10"
 )
 
 type Server struct {
@@ -14,6 +18,13 @@ type Server struct {
 func NewServer(store db.Store) *Server {
 	server := &Server{store: store}
 	router := gin.Default()
+
+	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
+		err := v.RegisterValidation("currency", validCurrency)
+		if err != nil {
+			log.Fatal("failed to register validator:", err)
+		}
+	}
 
 	router.POST("/accounts", server.createAccount)
 	router.GET("/accounts/:id", server.getAccount)
